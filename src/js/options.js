2ef50446b011589b680282a92eca8b3f1570659c
William Sinn <w.h.sinn@gmail.com>
2019-06-15 07:42:20 -0400

    listener may need to be defined in popup

--- src/js/options.js
+++ src/js/options.js
@@ -1,30 +1,36 @@
 import "../css/options.css";
 const grab = (selectorStr, parent = document) =>  parent.querySelector(selectorStr)
-const optionsURL="http://localhost:3001/options.html"
 const userId = 1;
 const notesURL="http://localhost:3000/notes/";
+const optionsURL="http://localhost:3001/options.html"
 
-const folderNotesList = grab("#folder-notes-list")
-const noteContainer = grab("#note-container")
-const urlContainer = grab("#url-container")
-const btnContainer = grab("#btn-container")
+
+
 
 export default function() {
-  let view=[];
+
+  document.addEventListener("DOMContentLoaded", event => {
+
+
+
   folderNotesList.addEventListener('click', event => {
     const clickTarget = event.target.id;
     let noteId = parseInt(clickTarget.split("-")[1]);
+
     if (clickTarget === `note-${noteId}`) {
-  return fetch(notesURL+`${noteId}`)
-  .then(r => r.json())
-  .then(note => {return view.push(note)})
-      }
-  })
-  console.log(view);
-  // noteContainer.innerHTML+=`<p>${note.note}</p>`;
-  // urlContainer.innerHTML+=`<p>${note.note}</p>`;
-  // btnContainer.innerHTML+=`<button id=edit-${note.id}>"EDIT"<button>`
-  // createWindow()
+
+    const view = fetch(notesURL+`${noteId}`)
+                  .then(r => r.json())
+                  .then(note => {
+                    return note
+
+              })}
+            })
+
+
+
+          })
+          createWindow()
 
 
     // console.log('%c <-- options.js --> ', 'background: #222; color: orange')
@@ -38,6 +44,6 @@ export default function() {
 
 
 const createWindow = () => {
-  chrome.windows.create({url: chrome.runtime.getURL("options.html"), type:"popup", state:"normal", height:"inherit", width:"inherit"}, function(){
+  chrome.windows.create({url: chrome.runtime.getURL("options.html"), type:"popup", state:"normal", height:400, width:400}, function(){
   })
 }
