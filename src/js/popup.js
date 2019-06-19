04a56cb0d8855dc84f700c023a2ae9a03fe9f3d9
ferryrules <ferris@ferryrules.com>
2019-06-19 09:20:37 -0400

    popup/popup.js

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,15 +1,29 @@
 import "../css/popup.css";
 import renderNotes from "./popup/notes"
-
-
+import options from './options.js'
+const grab = (selectorStr, parent = document) =>  parent.querySelector(selectorStr)
+const folderNotesList = grab("#folder-notes-list")
 
 document.addEventListener("DOMContentLoaded", event => {
+  renderNotes()
 
   console.log('%c <-- popup.js --> ', 'background: #222; color: green', event);
-  const open = event.target.webkitVisibilityState
-  if (open === "visible") {
-    renderNotes()
+  const isOpen = event.target.webkitVisibilityState
+  if (isOpen === "visible") {
+    options()
   }
 })
 
 // chrome.app.window.current().onClosed.addListener(event => {console.log()})
+
+
+
+
+// document.addEventListener("DOMContentLoaded", event => {
+//
+//   console.log('%c <-- popup.js --> ', 'background: #222; color: green', event);
+//   const open = event.target.webkitVisibilityState
+//   if (open === "visible") {
+//     renderNotes()
+//   }
+// })
